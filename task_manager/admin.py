0872ee258ae3464715810fffc284c4b7cf27382d
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-06-25 18:23:56 +0300

    setup admin site for task manager

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -3,28 +3,89 @@ from django.contrib import admin
 from .models import Goal, Task, TaskNote
 
 
+class TaskInLine(admin.StackedInline):
+    model = Task
+    extra = 1
+
+
+class NoteInLine(admin.StackedInline):
+    model = TaskNote
+    extra = 1
+
+
 class GoalAdmin(admin.ModelAdmin):
-    fields = [
+    fields = (
         'title',
         'content',
         'is_done',
-    ]
-    list_display = [
+    )
+    list_display = (
         'title',
         'created_at',
         'is_done',
-    ]
-    list_filter = [
+    )
+    list_filter = (
         'is_done',
         'created_at',
         'updated_at'
-    ]
-    search_fields = [
+    )
+    search_fields = (
         'title',
+    )
+    list_per_page = 20
+
+    inlines = [
+        TaskInLine,
     ]
+
+
+class TaskAdmin(admin.ModelAdmin):
+    fields = (
+        'title',
+        'content',
+        'deadline',
+        'status',
+        'goal',
+        )
+    list_display = (
+        'title',
+        'status',
+    )
+    list_filter = (
+        'status',
+        'created_at',
+        'updated_at',
+        'deadline'
+    )
+    search_fields = (
+        'title',
+        'status',
+    )
+    list_per_page = 20
+    inlines = [
+        NoteInLine,
+    ]
+
+
+class NoteAdmin(admin.ModelAdmin):
+    fields = (
+        'title',
+        'text',
+        'task',
+        )
+    list_display = (
+        'title',
+    )
+    list_filter = (
+        'created_at',
+        'updated_at',
+    )
+    search_fields = (
+        'title',
+    )
     list_per_page = 20
 
 
 admin.site.register(Goal, GoalAdmin)
-admin.site.register(Task)
-admin.site.register(TaskNote)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(TaskNote, NoteAdmin)
