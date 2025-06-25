3e7a17c03c0440df99292ff17055fa805eb01faa
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-06-25 13:24:36 +0300

    add views for and urlf for tasks manager

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
-import views
+from .views import goals_and_tasks
+from .views import AllTaskView, DetailTaskView
+from .views import AllGoalsView, DetailGoalView
 
 
 urlpatterns = [
+    path('', goals_and_tasks, name='task_home'),
 
+    path('tasks/', AllTaskView.as_view(), name='tasks-page'),
+    path('tasks/<uuid:task_id>/', DetailTaskView.as_view(), name='single-task'),
+
+    path('goals/', AllGoalsView.as_view(), name='goals-page'),
+    path('goals/<uuid:goal_id>/', DetailGoalView.as_view(), name='single-goal'),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,1 +1,40 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+
+from .models import Goal, Task
+
+
+def goals_and_tasks(request):
+    goals = Goal.objects.all()
+    tasks = Task.objects.filter(goal__exact=None)
+    data = {
+        'goals': goals,
+        'tasks': tasks
+    }
+    return render(request, '', data)
+
+
+class AllTaskView(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    paginate_by = 20
+    template_name = ''
+
+
+class DetailTaskView(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = ''
+
+
+class AllGoalsView(ListView):
+    model = Goal
+    context_object_name = 'goals'
+    paginate_by = 10
+    template_name = ''
+
+
+class DetailGoalView(DetailView):
+    model = Goal
+    context_object_name = 'goal'
+    template_name = ''
